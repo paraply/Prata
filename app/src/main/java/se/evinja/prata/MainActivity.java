b82a7bdeca7fad8054a9893842b622c8224d50b5
paraply <git@evinja.se>
2017-01-06 23:13:47 +0100

    Added firebase messaging and analytics

--- app/src/main/java/se/evinja/prata/MainActivity.java
+++ app/src/main/java/se/evinja/prata/MainActivity.java
@@ -1,13 +1,17 @@
 package se.evinja.prata;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
